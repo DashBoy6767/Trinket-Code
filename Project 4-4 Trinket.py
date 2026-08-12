a6d6b54bf0a244865b07276c34c632ab7b39a7b6
DashBoy6767 <darshmal617@gmail.com>
2026-08-12 12:30:19 -0700

    Create Project 4-4 Trinket.py

---
+++ Project 4-4 Trinket.py
@@ -0,0 +1,19 @@
+turtle=input("Action,Comedy,Horror,Science Fiction, or Drama(make sure to not capitalize it) ")
+
+'''The Dark Knight
+Monty Python and the Holy Grail
+The Shining
+Inception
+The Shawshank Redemption'''
+if turtle=="action":
+  print("The dark knight")
+elif turtle=="comedy":
+  print("monty pyhton and the holy grail")
+elif turtle=="horror":
+  print("the shining")  
+elif turtle=="science fiction":
+  print("inception")  
+elif turtle=="drama":
+  print("the shawshank redemption") 
+else:
+  print("not an option")
