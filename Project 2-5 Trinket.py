603157d64e794d9abd4a20d2471c25e84bdfd468
DashBoy6767 <darshmal617@gmail.com>
2026-08-12 10:41:28 -0700

    Add tip calculation based on bill amount and percentage

---
+++ Project 2-5 Trinket.py
@@ -0,0 +1,7 @@
+turtle=(int(input("Can I have your bill amount? ")))
+mouse=(int(input("Can I have your tip percentage? ")))
+
+elephant=mouse*0.01
+hippo=turtle*elephant
+tip=hippo+turtle
+print("$"+(str(tip)))
