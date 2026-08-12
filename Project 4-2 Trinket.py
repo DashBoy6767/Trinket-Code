c10a29c224de98808470661570ef9777f826c363
DashBoy6767 <darshmal617@gmail.com>
2026-08-12 12:09:43 -0700

    Add mood response program in Trinket.py

---
+++ Project 4-2 Trinket.py
@@ -0,0 +1,11 @@
+basic_moods = ["Happiness", "Sadness", "Anger", "Fear"]
+turtle=input("""Do you feel like Happiness, Sadness, Anger, or Fear """)
+
+if turtle=="happiness":
+  print("good for you ")
+elif turtle=="sadness":
+  print("bad for you ")
+elif turtle=="anger":
+  print("calm down ")
+elif turtle=="fear":
+  print("stop being a scaredy cat ")  
