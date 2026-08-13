f31d952afc502593190939431844ea3cc175bce4
DashBoy6767 <darshmal617@gmail.com>
2026-08-13 10:49:38 -0700

    Add turtle set manipulation functionality
    Initial implementation of a turtle set manipulation script.

---
+++ Project 7-1 Trinket.py
@@ -0,0 +1,14 @@
+'''turtle=set("turtle and stuff")
+print(turtle)
+turtle.add("turtle and stuff")
+print(turtle)
+turtle.remove("t")
+print(turtle)
+turtle.discard("t")
+print(turtle)'''
+turtle=input("give me a sentence")
+a=set(turtle)
+
+tutle=turtle.split()
+tutle=set(tutle)
+print(turtle)
