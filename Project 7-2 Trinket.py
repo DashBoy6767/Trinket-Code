c93e1db3bf3e5755c4b67a9598e7f98cef30d881
DashBoy6767 <darshmal617@gmail.com>
2026-08-13 10:50:06 -0700

    Add intersection and difference operations for sets

---
+++ Project 7-2 Trinket.py
@@ -0,0 +1,7 @@
+turtle={"tennis","dance","oral language"}
+tutle={"tennis","robotics","math"}
+print(turtle.intersection(tutle))
+print(turtle,"Darsh's")
+print(tutle,"Daiwik's")
+print(turtle-turtle.intersection(tutle))
+print(tutle-turtle.intersection(tutle))
