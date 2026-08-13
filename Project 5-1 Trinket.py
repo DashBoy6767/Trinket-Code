e204afef0a08c7e9fcf0b7986ef66cdec0d0f602
DashBoy6767 <darshmal617@gmail.com>
2026-08-13 10:45:58 -0700

    Add meal suggestion program based on user input

---
+++ Project 5-1 Trinket.py
@@ -0,0 +1,20 @@
+turtle=input("breakfast, lunch, or dinner")
+
+if turtle=="breakfast":
+  a=input("veggie or meat ")
+  if a=="veggie":
+    print("you get cereal and banana")
+  elif a=="meat":
+    print("you get sausage and an orange")
+elif turtle=="lunch":
+  a=input("veggie or meat ")
+  if a=="veggie":
+    print("you get rice and beans")
+  elif a=="meat":
+    print("you get a chicken sandwich")    
+if turtle=="dinner":
+  a=input("veggie or meat ")
+  if a=="veggie":
+    print("you get mashed potatoes and salad")
+  elif a=="meat":
+    print("you get turkey")    
